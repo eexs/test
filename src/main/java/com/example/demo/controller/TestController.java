d4bbe806a7f27cdddf086a605b0046ee485b655c
zhouwenbin <wenbin.zhou@opene.com>
2021-08-25 14:53:16 +0800

    test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-import com.dianping.cat.Cat;
-import com.dianping.cat.message.Event;
-import com.dianping.cat.message.Transaction;
 import com.example.demo.entity.TestEntity;
 import com.example.demo.service.TestService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,30 +27,6 @@ public class TestController {
     public ResponseEntity getAll() {
         List<TestEntity> testEntityList = testService.getAll();
         return ResponseEntity.ok(testEntityList);
-    }
-
-    @GetMapping("/cat")
-    public void cat() {
-        Transaction t = Cat.newTransaction("URL", "/test/cat");
-        try {
-            Cat.logEvent("URL.Server", "serverIp", Event.SUCCESS, "ip=${serverIp}");
-            Cat.logMetricForCount("metric.key");
-            Cat.logMetricForDuration("metric.key", 5);
-
-            yourBusiness();
-
-            t.setStatus(Transaction.SUCCESS);
-        } catch (Exception e) {
-            t.setStatus(e);
-            Cat.logError(e);
-        } finally {
-            t.complete();
-        }
-    }
-
-    private void yourBusiness() {
-//        int i = 10 / 0;
-        System.out.println("test cat!");
     }
 
 }
